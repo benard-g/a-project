f8d9b182fd626fb8583ff3543c660ffc657d7915
Guillaume BENARD <guillaume@benard.io>
2021-07-18 15:00:21 +0200

    feat(fastify): retrieve PORT from env

--- packages/api-gateway/src/config/index.ts
+++ packages/api-gateway/src/config/index.ts
@@ -1,10 +1,15 @@
 import { getNodeEnv } from './getNodeEnv';
+import { getNumberFromEnv } from './utils/env';
+
+const DEFAULT_PORT = 8080;
 
 export function loadConfig() {
   const nodeEnv = getNodeEnv();
+  const isDevMode = nodeEnv !== 'production';
 
   return {
     NODE_ENV: nodeEnv,
+    PORT: getNumberFromEnv('PORT', isDevMode ? DEFAULT_PORT : undefined),
   };
 }
 
