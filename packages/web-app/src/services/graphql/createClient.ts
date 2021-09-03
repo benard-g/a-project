e35aee18f7b4f8c0c0e5d2bf5b131c6284f7dd9c
Guillaume Bénard <guillaume@benard.io>
2021-09-03 12:02:38 +0200

    chore(apollo): enable dev-tools in dev-mode

--- packages/web-app/src/services/graphql/createClient.ts
+++ packages/web-app/src/services/graphql/createClient.ts
@@ -3,10 +3,12 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { CONFIG } from '../../config';
 
 const GRAPHQL_URL = `${CONFIG.API_URL}/graphql`;
+const IS_DEV_MODE = CONFIG.NODE_ENV === 'development';
 
 export function createClient() {
   return new ApolloClient({
     cache: new InMemoryCache({}),
+    connectToDevTools: IS_DEV_MODE,
     uri: GRAPHQL_URL,
   });
 }
